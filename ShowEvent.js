acb511a031a6ad01ad339496213c227dca388849
msolitano <39394958+msolitano@users.noreply.github.com>
2018-05-31 17:15:55 -0700

    Update ShowEvent.js

--- ShowEvent.js
+++ ShowEvent.js
@@ -1,12 +1,40 @@
 class ShowEvent extends React.Component {
     constructor(props) {
       super(props);
+      this.deleteEvent = this.deleteEvent.bind(this);
+      this.getEvent = this.getEvent.bind(this);
       this.state = {
         events:[]
       };
     }
 
+    deleteEvent(id) {
+        if(confirm('Are you sure ?')){
+        var self = this;
+        axios.post('/deleteEvent', {
+          id: id
+        })
+        .then(function (response) {
+          self.getEvent();
+        })
+        .catch(function (error) {
+          console.log('Error is ',error);
+        });
+       }
+    }
     
+    getEvent(id) {
+      var self = this;
+      axios.post('/getEvent', {
+        })
+      .then(function (response) {
+        console.log('res is ',response);
+        self.setState({events:response.data})
+        })
+      .catch(function (error) {
+        console.log('error is ',error);
+        });
+    }
 
     componentDidMount(){
       var self = this;
@@ -37,6 +65,7 @@ class ShowEvent extends React.Component {
                           <p classname="list-group-item-text">{event.date}</p>
                           <p className="list-group-item-text">{event.description}</p>
                           <p className="list-group-item-text">{event.addData}</p>
+                          <button onClick={this.deleteEvent.bind(this,post._id)}>Delete Event</button>
                         </a>
               })
             }
